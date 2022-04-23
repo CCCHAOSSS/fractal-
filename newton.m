7fe5bfc3fa1c704eca03b58427d36fc951590bef
CCCHAOSSS <58129786+CCCHAOSSS@users.noreply.github.com>
2022-04-23 10:04:42 +0800

    单函数控制的牛顿迭代

--- newton.m
+++ newton.m
@@ -8,10 +8,10 @@ for j=1:num
      for k=1:num 
          n=1; 
          z0=x(j)+y(k)*1i; 
-         z=z0+z0^20;                %随机选择一个初始值，只要保证满足abs(z-z0)
+         z=z0-(z0^5-1)/(5*z0^4);                
          while abs(z-z0)>10e-6 && n<n_max 
              z0=z; 
-            z=z0-(z0^2-1)/(2*z0);     %newton迭代公式
+            z=z-(z0^5-1)/(5*z0^4);     %newton迭代公式
              n=n+1; 
          end 
          M(k,j)=n; 
@@ -20,6 +20,6 @@ end
 surf(X,Y,M); 
 view(0,-90); 
 shading flat 
-colormap(gray); 
+colormap(flipud(jet)); 
 colorbar; 
 axis equal
